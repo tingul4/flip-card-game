3eb0652d9d5aa08e7f936b6e7237c5c0461cee95
tingul4 <xpple413208@gmail.com>
2023-06-06 11:53:33 +0800

    add flip card function

--- main.js
+++ main.js
@@ -4,28 +4,18 @@ const Symbols = [
   'https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png', // 方塊
   'https://assets-lighthouse.alphacamp.co/uploads/image/file/17988/__.png' // 梅花
 ]
-
-const utility = {
-  getRandomNumberArray (count) {
-    const number = Array.from(Array(count).keys())
-    for (let index = number.length - 1; index > 0; index--) {
-      let randomIndex = Math.floor(Math.random() * (index + 1))
-        ;[number[index], number[randomIndex]] = [number[randomIndex], number[index]]
-    }
-    return number
-  }
-}
-
 const view = {
   getCardElement (index) {
+    return `<div data-index="${index}" class="card back"></div>`
+  },
+  getCardContent (index) {
     const number = this.transformNumber((index % 13) + 1)
     const symbol = Symbols[Math.floor(index / 13)]
     return `
-      <div class="card">
-        <p>${number}</p>
-        <img src="${symbol}" />
-        <p>${number}</p>
-      </div>`
+      <p>${number}</p>
+      <img src="${symbol}" />
+      <p>${number}</p>
+    `
   },
   transformNumber (number) {
     switch (number) {
@@ -44,7 +34,32 @@ const view = {
   displayCards () {
     const rootElement = document.querySelector('#cards')
     rootElement.innerHTML = utility.getRandomNumberArray(52).map(index => this.getCardElement(index)).join('')
+  },
+  flipCard (card) {
+    if (card.classList.contains('back')) {
+      // 回傳正面
+      card.classList.remove('back')
+      card.innerHTML = this.getCardContent(Number(card.dataset.index))
+      return
+    }
+    // 回傳背面
+    card.classList.add('back')
+    card.innerHTML = null
   }
 }
-
+const utility = {
+  getRandomNumberArray (count) {
+    const number = Array.from(Array(count).keys())
+    for (let index = number.length - 1; index > 0; index--) {
+      let randomIndex = Math.floor(Math.random() * (index + 1))
+        ;[number[index], number[randomIndex]] = [number[randomIndex], number[index]]
+    }
+    return number
+  }
+}
 view.displayCards()
+document.querySelectorAll('.card').forEach(card => {
+  card.addEventListener('click', event => {
+    view.flipCard(card)
+  })
+})
